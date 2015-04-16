8e89243b9c91deb2e511451ee4e05b7d63e8a62f
Kathryn Carr <kathrynelizabethcarr@gmail.com>
2015-04-16 16:30:45 -0700

    Begin to add function to aggregate data

---
+++ scripts.js
@@ -0,0 +1,25 @@
+var groupData = function(data){
+
+  var groupedData = [];
+
+  for(var i = 0; i < 30; i++){
+    for(var j = 0; j < 24; j++){
+      groupedData.push({day: i, hour: j ,total: 0});
+    }
+  }
+
+  var tempDate;
+
+  for(var i = 0; i < groupedData.length; i++){
+    data.forEach(function(eventObj){
+      tempDate = new Date(eventObj.timestamp);
+
+      if(groupedData[i].day === tempDate.getDate() && groupedData[i].hour === tempDate.getHours()){
+        groupedData[i].total = groupedData[i].total + 1;
+      }
+    });
+  }
+
+// console.log(groupedData);
+  return groupedData;
+};
